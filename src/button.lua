48830e1653f29d8000783adb606633d21dd9e354
Damien CAROL <damien.carol@gmail.com>
2012-09-12 12:02:16 +0200

    Improved button rendering

--- src/button.lua
+++ src/button.lua
@@ -9,7 +9,7 @@ function button_load()
 	--button_tilesetBatch = love.graphics.newSpriteBatch(gui.image_gui, 20 * 50)
 	--button_quad = love.graphics.newQuad(500, 500, 300, 80, gui.image_gui:getWidth(), gui.image_gui:getHeight())
 
-	Button.font = love.graphics.newFont("fnt/Knight2.ttf", 32)
+	Button.font = love.graphics.newFont("fnt/Knight2.ttf", 24)
 end
 
 function Button.create()
@@ -28,23 +28,29 @@ end
 
 
 function Button:draw(offset_x, offset_y)
-	local button_quad
-	button_quad = love.graphics.newQuad(864, 396, 30, 30, 
+
+	local button_quad = love.graphics.newQuad(864, 396, 30, 30, 
 		gui.image_gui:getWidth(), gui.image_gui:getHeight())
 
-	border_left_quad = love.graphics.newQuad(864, 428, 30, 50, 
+	local border_left_quad = love.graphics.newQuad(864, 428, 30, 50, 
 		gui.image_gui:getWidth(), gui.image_gui:getHeight())
 
-	border_right_quad = love.graphics.newQuad(864, 428, 30, 50, 
+	local border_right_quad = love.graphics.newQuad(982, 428, 30, 50, 
 		gui.image_gui:getWidth(), gui.image_gui:getHeight())
 
-		
-		
-	back_quad = love.graphics.newQuad(0, 0, self.size_x, self.size_y, 
+	local border_up_quad = love.graphics.newQuad(899, 396, 70, 30, 
+		gui.image_gui:getWidth(), gui.image_gui:getHeight())
+
+	local border_down_quad = love.graphics.newQuad(899, 498, 70, 30, 
+		gui.image_gui:getWidth(), gui.image_gui:getHeight())
+
+
+
+	local back_quad = love.graphics.newQuad(0, 0, self.size_x, self.size_y, 
 		gui.image_gui:getWidth(), gui.image_gui:getHeight())
 
 	-- hover effect
-		if not self.hover then
+	if not self.hover then
 		love.graphics.setColor(192, 192, 192, 255 )
 	else
 		love.graphics.setColor(255, 255, 255, 255 )
@@ -62,13 +68,52 @@ function Button:draw(offset_x, offset_y)
 		end
 		local rd_left_quad = love.graphics.newQuad(864, 428, 30, nb_rd,
 			gui.image_gui:getWidth(), gui.image_gui:getHeight())
-		love.graphics.drawq(gui.image_gui, border_left_quad, offset_x + self.x - 6, offset_y + self.y + (50 * (nb-1)))
+		love.graphics.drawq(gui.image_gui, rd_left_quad, offset_x + self.x - 6, offset_y + self.y + (50 * (nb)) - nb_rd)
 		
-		-- upper left corner
-		love.graphics.drawq(gui.image_gui, button_quad, offset_x + self.x - 6, offset_y + self.y - 6)
 	end
 
+	-- right border
+	do
+		local nb = self.size_y / 50
+		local nb_rd = self.size_y % 50
+		for i=0,(nb-1) do
+			love.graphics.drawq(gui.image_gui, border_right_quad, offset_x + self.size_x + self.x - 14, offset_y + self.y + (50 * i))
+		end
+		local rd_left_quad = love.graphics.newQuad(982, 428, 30, nb_rd,
+			gui.image_gui:getWidth(), gui.image_gui:getHeight())
+		love.graphics.drawq(gui.image_gui, rd_left_quad, offset_x + self.size_x + self.x - 14, offset_y + self.y + (50 * (nb)) - nb_rd)
+		
+	end
 
+	-- up border
+	do
+		local nb = self.size_x / 70
+		local nb_rd = self.size_x % 70
+		for i=0,(nb-1) do
+			love.graphics.drawq(gui.image_gui, border_up_quad, offset_x + self.x  + (70 * i), offset_y + self.y - 6)
+		end
+		local rd_left_quad = love.graphics.newQuad(899, 396, nb_rd, 30,
+			gui.image_gui:getWidth(), gui.image_gui:getHeight())
+		love.graphics.drawq(gui.image_gui, rd_left_quad, offset_x + self.x + (70 * (nb)) - nb_rd, offset_y + self.y - 6)
+		
+	end
+
+	-- down border
+	do
+		local nb = self.size_x / 70
+		local nb_rd = self.size_x % 70
+		for i=0,(nb-1) do
+			love.graphics.drawq(gui.image_gui, border_down_quad, offset_x + self.x  + (70 * i), offset_y + self.size_y + self.y - 14)
+		end
+		local rd_left_quad = love.graphics.newQuad(899, 498, nb_rd, 30,
+			gui.image_gui:getWidth(), gui.image_gui:getHeight())
+		love.graphics.drawq(gui.image_gui, rd_left_quad, offset_x + self.x + (70 * (nb)) - nb_rd, offset_y + self.size_y + self.y - 14)
+		
+	end
+
+	-- upper left corner
+	love.graphics.drawq(gui.image_gui, button_quad, offset_x + self.x - 6, offset_y + self.y - 6)
+	
 	-- text
 	love.graphics.setFont(Button.font)
 	
